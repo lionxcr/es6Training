a88ef7c3e20dc9333051282dc9903a38ca0a26c4
Pablo Segura <pablos@thehangar.cr>
2016-12-19 14:16:09 -0600

    Added data

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import fs from 'fs';
 
 (async () => {
     try{
         const port = 8080;
         const app = express();
+        const data = JSON.parse(fs.readFileSync('file', 'utf8'));
+        console.log(data);
         app.use('/' (req, res) => {
         	res.render('hello world');
         });
